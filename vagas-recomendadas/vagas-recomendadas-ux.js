3482e20723fb2976b09a18a23943a0a5799ee134
viniciusmrs <41207331+viniciusmrs@users.noreply.github.com>
2026-09-16 15:57:57 -0300

    Limpar script da tela vagas recomendadas

--- vagas-recomendadas/vagas-recomendadas-ux.js
+++ vagas-recomendadas/vagas-recomendadas-ux.js
@@ -1,11 +1,5 @@
-const guideButton = document.getElementById("showGuide");
-const guidePanel = document.getElementById("guidePanel");
 const filterButtons = document.querySelectorAll(".filters-used button:not(.clear)");
 const clearButton = document.querySelector(".filters-used .clear");
-
-guideButton.addEventListener("click", () => {
-  guidePanel.scrollIntoView({ behavior: "smooth", block: "start" });
-});
 
 filterButtons.forEach((button) => {
   button.addEventListener("click", () => button.remove());
